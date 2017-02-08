d87cc83c0ac3927da50754ea4a3c38709a17227b
Preeti Ahuja <preeti.ap3@gmail.com>
2017-02-07 20:07:44 -0800

    first commit

---
+++ InternetApp/Models/NorthwindPartials/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace InternetApp.Models.Northwind
+{
+    [MetadataType(typeof(CustomerMetaData))]
+    public partial class Customer { }
+   
+    public class CustomerMetaData
+    {
+        [Required]
+        [Display(Name = "Company")]
+        public object CompanyName { get; set; }
+
+        [Required]
+        [Display(Name = "Contact")]
+        public object ContactName { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        public object ContactTitle { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public object City { get; set; }
+
+    }
+}
---
+++ InternetApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetApp.Startup))]
+namespace InternetApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
